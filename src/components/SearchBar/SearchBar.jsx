8f6f80e87cf9efc52e81978e6a40de66465f033d
Pamella Binotto <pamellabinotto@gmail.com>
2026-07-10 23:11:26 -0300

    feat(search): create search bar component structure

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,13 @@
-function SearchBar(){
-    return(
-        <div> Search bar</div>
+function SearchBar() {
+    return (
+        <div className="flex items-center p-4">
+
+            <input className="flex-1" type="text" placeholder="🔍 Buscar cidade ..." />
+
+            <button> 📍 </button>
+            <button> 👤 </button>
+
+        </div>
     );
 }
 
