ce068342754de26bbf4ef50fbeb51daeb03a822d
Pamella Binotto <pamellabinotto@gmail.com>
2026-07-14 21:50:18 -0300

    feat(weather): implement weather service integration

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,15 +1,11 @@
-import weatherService from "../../services/weatherService"
-
-function WeatherCard({weather}){
-    return(
+function WeatherCard({ weather }) {
+    return (
         <div>
-
             <h2>{weather.city}</h2>
-            <h3>{weather.temperatura}</h3>
-            <h3></h3>
+            <h3>{weather.temperature}°C</h3>
+            <p>Umidade: {weather.humidity}%</p>
+            <p>Vento: {weather.windSpeed} km/h</p>
+            <p>{weather.alert}</p>
         </div>
-
     );
 }
-
-export default WeatherCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,14 +4,34 @@ import WeatherCard from "../../components/WeatherCard/WeatherCard";
 import RecommendationCard from "../../components/RecommendationCard/RecommendationCard";
 import ForecastCard from "../../components/ForecastCard/ForecastCard";
 
+import { useEffect, useState } from "react";
+import { getCurrentWeather } from "../../services/weatherService";
 
 
 function Home() {
+
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+
+        async function loadWeather() {
+
+            const data = await getCurrentWeather("São Paulo");
+
+            setWeather(data)
+
+        }
+
+        loadWeather();
+
+    }, []);
+
+
     return (
         <>
             <Header />
             <SearchBar />
-            <WeatherCard />
+            {weather && <WeatherCard weather={weather} />}
             <RecommendationCard />
             <ForecastCard />
         </>
