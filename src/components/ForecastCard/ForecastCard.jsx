1a7cf03b883009d1b16948ba8edcfa127ba882da
Pamella Binotto <pamellabinotto@gmail.com>
2026-07-17 20:06:10 -0300

    feat(forecast): render detailed 5-day weather forecast

--- src/components/ForecastCard/ForecastCard.jsx
+++ src/components/ForecastCard/ForecastCard.jsx
@@ -10,9 +10,15 @@ function ForecastCard({ forecast }) {
 
                     <h3>{formatDate(day.date)}</h3>
 
+                    <p>Humidity: {day.humidity}</p>
                     <p>Max: {day.maxTemp.toFixed(1)}°C </p>
                     <p>Min: {day.minTemp.toFixed(1)}°C </p>
                     <p>Rain: {day.rainProbability}% </p>
+                    <p>Rain Alert: {day.rainAlert}</p>
+                    <p>Wind: {day.windSpeed.toFixed(1)} km/h</p>
+                    <p>Wind Alert: {day.windAlert}</p>
+
+                    <br />
 
                 </div>
 
